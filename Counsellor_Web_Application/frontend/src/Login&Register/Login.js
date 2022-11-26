d1237431d98bd0261588d6653164b56b42c62b06
Abdul Nafay <101311065+Nafay1010@users.noreply.github.com>
2022-11-26 05:59:38 +0500

    Add files via upload

---
+++ Counsellor_Web_Application/frontend/src/Login&Register/Login.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const {login, error, isLoading} = useLogin()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await login(email, password)
+  }
+
+  return (
+    <div className="login-div">
+    <form className="login-form" onSubmit={handleSubmit}>
+        <h1>Log In</h1>
+        <br />
+        <div>
+          <label>Email address:</label>
+          <input 
+            required
+            type="email" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email} 
+            />
+        </div>
+        <br /><br />
+        <div>
+        <label>Password:</label>
+        <input 
+          required
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+          />
+        </div>
+        <br /><br />
+        <div className="button">
+          <button disabled={isLoading}>Log in</button>
+          {error && <div className="error">{error}</div>}
+        </div>
+      </form>
+    </div>
+
+  )
+}
+
+export default Login
